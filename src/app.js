6011f0c914d0296831a065a83e5a0cc01fe50d52
iam-abhishek-yadav <abhrecscs17@gmail.com>
2026-05-26 20:29:14 +0530

    Add register route

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import morgan from "morgan";
+import authRouter from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+
+export default app;
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+import UserModel from "../models/user.model.js";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
+export async function register(req, res) {
+  const { username, email, password } = req.body;
+
+  const isAlreadyRegistered = await UserModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isAlreadyRegistered) {
+    return res.status(409).json({
+      message: "Username or email already exists.",
+    });
+  }
+
+  const hashedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+
+  const user = await UserModel.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.status(201).json({
+    message: "User registered successfully.",
+    user: {
+      username: user.username,
+      email: user.email,
+      token: token,
+    },
+  });
+}
